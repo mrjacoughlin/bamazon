a2952fb9135eb5df3729c77b08a89b63dd8e7d44
James Coughlin <mrjacoughlin@gmail.com>
2021-01-05 16:49:31 -0500

    function added if the user was successful or not

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,24 +18,54 @@ connection.connect(function (err) {
 function start() {
   connection.query(" SELECT * FROM products", function (err, results) {
     if (err) throw err;
-    inquirer.prompt([
-      {
-        name: "choice",
-        type: "rawlist",
-        choices: function () {
-          var choiceArray = [];
-          for (var i = 0; i < results.length; i++) {
-            choiceArray.push(results[i].product_name);
-          }
-          return choiceArray;
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function () {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: " What item would you like to buy",
         },
-        message: " What item would you like to buy",
-      },
-      {
-        name: "units",
-        type: "input",
-        message: "How many units would you like to buy",
-      },
-    ]);
+        {
+          name: "units",
+          type: "input",
+          message: "How many units would you like to buy",
+        },
+      ])
+      .then(function (answer) {
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+        if (chosenItem.stock_quantity < parseInt(answer.unit)) {
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: answer.units,
+              },
+              {
+                item_id: chosenItem.item_id,
+              },
+            ],
+            function (err) {
+              if (err) throw err;
+              console.log("Your Purchase was Succesful");
+              start();
+            }
+          );
+        } else {
+          console.log("Not enough units availible. Try a different amount");
+          start();
+        }
+      });
   });
 }
