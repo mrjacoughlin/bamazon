1a68d0f7744e2b85b9c867cd4c0a8371473f1020
James Coughlin <mrjacoughlin@gmail.com>
2021-01-06 12:23:04 -0500

    Updated syntax, to loadProducts. Changing inquirer to pass through new function promptcustomerforitem

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,62 +10,69 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
 
-  start();
+  loadProducts();
 });
 
-function start() {
-  connection.query(" SELECT * FROM products", function (err, results) {
+function loadProducts() {
+  connection.query(" SELECT * FROM products", function (err, res) {
     if (err) throw err;
-    inquirer
-      .prompt([
-        {
-          name: "choice",
-          type: "rawlist",
-          choices: function () {
-            var choiceArray = [];
-            for (var i = 0; i < results.length; i++) {
-              choiceArray.push(results[i].product_name);
-            }
-            return choiceArray;
-          },
-          message: " What item would you like to buy",
-        },
-        {
-          name: "units",
-          type: "input",
-          message: "How many units would you like to buy",
-        },
-      ])
-      .then(function (answer) {
-        var chosenItem;
-        for (var i = 0; i < results.length; i++) {
-          if (results[i].product_name === answer.choice) {
-            chosenItem = results[i];
-          }
-        }
-        if (chosenItem.stock_quantity < parseInt(answer.unit)) {
-          connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-              {
-                stock_quantity: answer.units,
-              },
-              {
-                item_id: chosenItem.item_id,
-              },
-            ],
-            function (err) {
-              if (err) throw err;
-              console.log("Your Purchase was Succesful");
-              start();
-            }
-          );
-        } else {
-          console.log("Not enough units availible. Try a different amount");
-          start();
-        }
-      });
+    //console.table gives you the table of results in the terminal
+    console.table(results);
+    //This promts the user for their choice and passes them through promptCustomerForItem(res)
+    promptCustomerForItem(res);
   });
 }
+//   inquirer
+//     .prompt([
+//       {
+//         name: "choice",
+//         type: "rawlist",
+//         choices: function () {
+//           var choiceArray = [];
+//           for (var i = 0; i < results.length; i++) {
+//             choiceArray.push(results[i].product_name);
+//           }
+//           return choiceArray;
+//         },
+//         message: " What item would you like to buy",
+//       },
+//       {
+//         name: "units",
+//         type: "input",
+//         message: "How many units would you like to buy",
+//       },
+//     ])
+//     .then(function (answer) {
+//       var chosenItem;
+//       for (var i = 0; i < results.length; i++) {
+//         if (results[i].product_name === answer.choice) {
+//           chosenItem = results[i];
+//         }
+//       }
+//       if (chosenItem.stock_quantity < parseInt(answer.unit)) {
+//         connection.query(
+//           "UPDATE products SET ? WHERE ?",
+//           [
+//             {
+//               stock_quantity: answer.units,
+//             },
+//             {
+//               item_id: chosenItem.item_id,
+//             },
+//           ],
+//           function (err) {
+//             if (err) throw err;
+//             console.log("Your Purchase was Succesful");
+//             start();
+//           }
+//         );
+//       } else {
+//         console.log("Not enough units availible. Try a different amount");
+//         start();
+//       }
+//     });
+// });
